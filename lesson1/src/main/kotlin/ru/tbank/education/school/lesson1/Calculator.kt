d9868cd584b79c907887f17f0c655d8bd4e7eea2
Анчуков Дмитрий Васильевич <dva01082009@gmail.com>
2025-09-12 17:43:23 +0300

    Update Calculator.kt

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -2,6 +2,11 @@ package ru.tbank.education.school.lesson1
 
 fun calculate(s: String): Any {
     val m = s.split(" ")
+    if (m.size != 3) {
+        val a = m[0].toInt()
+        val b = m[1].toInt()
+        return a + b
+    }
     val a = m[0].toInt()
     val b = m[2].toInt()
     val z = m[1]
