a546157ee73dd5249e1464473b9bd8eedbf94a66
Анчуков Дмитрий Васильевич <dva01082009@gmail.com>
2025-09-12 17:38:20 +0300

    Update Calculator.kt

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,39 +1,43 @@
 package ru.tbank.education.school.lesson1
 
+fun calculate(s: String): Any {
+    val m = s.split(" ")
+    val a = m[0].toInt()
+    val b = m[2].toInt()
+    val z = m[1]
+    return when (z) {
+        "+" -> a + b
+        "-" -> a - b
+        "*" -> a * b
+        "/" -> {
+            if (b == 0)
+                "null"
+            else
+                a.toDouble() / b.toDouble()
+        }
+
+        "**" -> {
+            var w: Long = 1
+            for (i in 0..<b) {
+                w *= a.toLong()
+            }
+            w
+        }
+
+        else -> {
+            "Функция отсутствует"
+        }
+    }
+}
+
 fun main(){
-    var s = "5 + 3"
-    while (s != "exit") {
-        println("""Введите через пробел. Пример: 5 + 3.
+    println("""Введите через пробел. Пример: 5 + 3.
             |Для выхода введите exit.
         """.trimMargin())
-        s = readln()
-        val m = s.split(" ")
-        val a = m[0].toInt()
-        val b = m[2].toInt()
-        val z = m[1]
-        val r = when (z) {
-            "exit" -> break
-            "+" -> a + b
-            "-" -> a - b
-            "*" -> a * b
-            "/" -> {
-                if (b==0)
-                    "null"
-                else
-                    a.toDouble() / b.toDouble()
-            }
-            "**" -> {
-                var w: Long = 1
-                for (i in 0..<b) {
-                    w *= a
-                }
-                w
-            }
-
-            else -> {
-                println("Функция отсутствует")
-            }
-        }
+    while (true) {
+        val s = readln()
+        if (s == "exit") break
+        val r = calculate(s)
         println(r)
     }
 }
