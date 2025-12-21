cb7e1b922e9c0f69559d2e5dd7de174387e6ae39
aDiamant03 <dva01082009@mail.ru>
2025-12-21 18:28:03 +0300

    дз
    Signed-off-by: aDiamant03 <dva01082009@mail.ru>

--- spring-lesson11/scripts.sql
+++ spring-lesson11/scripts.sql
@@ -18,7 +18,6 @@ create table public.account
         check (amount >= 0)
 );
 
--- Изменение таблицы
 alter table public.users
     add age integer;
 
@@ -26,54 +25,42 @@ alter table public.users
     add constraint check_age
         check (age > 0);
 
--- Удаление таблицы
-drop table if exists public.users;
+insert into public.users (id, last_name, first_name)
+values (default, 'Иванов', 'Иван');
 
+insert into public.users (id, last_name, first_name)
+values (default, 'Петров', 'Петр');
 
+insert into public.users (id, last_name, first_name)
+values (default, 'Петров2', 'Петр2');
 
--- Отображение и запись пользователей
-SELECT t.*
-FROM public.users t;
+update public.users
+set first_name = 'Дмитрий'
+where id = 3;
 
-INSERT INTO public.users (id, last_name, first_name) VALUES (DEFAULT, 'Иванов'::varchar(255), 'Иван'::varchar(255));
-INSERT INTO public.users (id, last_name, first_name) VALUES (DEFAULT, 'Петров'::varchar(255), 'Петр'::varchar(255));
-INSERT INTO public.users (id, last_name, first_name) VALUES (DEFAULT, 'Петров2'::varchar(255), 'Петр2'::varchar(255));
+delete from public.users
+where first_name = 'Дмитрий';
 
-UPDATE public.users SET first_name = 'Дмитрий'::varchar(255) WHERE id = 3::bigint;
+insert into public.account (id, user_id, amount)
+values (default, 1, 100);
 
-DELETE FROM public.users WHERE first_name = 'Дмитрий';
+insert into public.account (id, user_id, amount)
+values (default, 2, 200);
 
--- Отображение и запись счетов
-SELECT t.*
-FROM public.account t;
-
-INSERT INTO public.account (id, user_id, amount) VALUES (DEFAULT, 1::bigint, 100::bigint);
-INSERT INTO public.account (id, user_id, amount) VALUES (DEFAULT, 2::bigint, 200::bigint);
-
--- Выборка данных
-SELECT u.last_name, u.first_name, u.age, a.amount FROM users u, account a;
-SELECT * FROM users join account a on users.id = a.user_id;
-SELECT u.last_name, u.first_name, u.age, a.amount FROM users u join account a on u.id = a.user_id;
-
+select u.last_name, u.first_name, u.age, a.amount
+from users u
+join account a on u.id = a.user_id;
 
 begin transaction;
-insert into account(id, user_id, amount) values (default, 5, -100);
-commit;
-rollback ;
+insert into account(id, user_id, amount)
+values (default, 5, -100);
+rollback;
 
--- перевод денег со счета на счета
 begin transaction;
 update account set amount = amount - 10 where id = 1;
 update account set amount = amount + 10 where id = 2;
 commit;
 
--- добавим связь пользователя со счетом
 alter table account
     add constraint account_users_id_fk
         foreign key (user_id) references users;
-
--- создали счет, но забыли создать пользователя
-begin transaction;
-insert into account(id, user_id, amount) values (default, 99, 100);
-commit;
-rollback;
